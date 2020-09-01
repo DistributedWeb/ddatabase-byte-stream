4cea685a77b97c82893258d79af23936ae05927c
shikhars371 <shikhars371@gmail.com>
2020-09-01 20:07:44 +0530

    var changes

--- test/helpers/create.js
+++ test/helpers/create.js
@@ -1,10 +1,10 @@
 const ram = require('random-access-memory')
-const hypercore = require('hypercore')
+const ddatabase = require('ddatabase')
 
 const createStream = require('../..')
 
 function createLocal (numRecords, recordSize, cb) {
-  let core = hypercore(ram)
+  let core = ddatabase(ram)
 
   let records = []
   for (let i = 0; i < numRecords; i++) {
@@ -20,7 +20,7 @@ function createLocal (numRecords, recordSize, cb) {
 }
 
 function createRemote (numRecords, recordSize, cb) {
-  let core1 = hypercore(ram, { sparse: true })
+  let core1 = ddatabase(ram, { sparse: true })
 
   let records = []
   for (let i = 0; i < numRecords; i++) {
@@ -31,7 +31,7 @@ function createRemote (numRecords, recordSize, cb) {
   core1.append(records, err => {
     if (err) return cb(err)
 
-    let core2 = hypercore(ram, core1.key, { sparse: true })
+    let core2 = ddatabase(ram, core1.key, { sparse: true })
 
     let s1 = core1.replicate({ live: true })
     s1.pipe(core2.replicate({ live: true })).pipe(s1)
--- test/helpers/suite.js
+++ test/helpers/suite.js
@@ -145,7 +145,7 @@ module.exports = function (tag, create) {
     })
   })
 
-  test(`${tag}: stream with byteOffset, length larger than hypercore size should wait`, t => {
+  test(`${tag}: stream with byteOffset, length larger than ddatabase size should wait`, t => {
     create(10, 100, (err, input, output, stream, records) => {
       t.error(err, 'create stream ok')
       let combined = Buffer.concat(records.slice(5), 500)
